4825c141b347107109b9b4fc71b2cc2e11cd3118
Brian Wong <brianwong@zipcoders-MBP.fios-router.home>
2019-03-02 18:42:57 -0500

    1-3

--- src/main/java/rocks/zipcode/stringsgalore/StringTheory.java
+++ src/main/java/rocks/zipcode/stringsgalore/StringTheory.java
@@ -7,4 +7,21 @@ public class StringTheory {
 
     // Put your code for all 70 exercises starting here.
 
+    //1
+    public Character characterByIndex(String word, Integer index) {
+       return word.charAt(index);
+    }
+
+    //2
+    public Character characterUnicodeByIndex(String word, Integer index) {
+        return word.charAt(index);
+
+    }
+
+    //3
+    public Character characterUnicodeBeforeSpecifiedIndex(String word, Integer index) {
+        return word.charAt(index-1);
+
+    }
+
 }
--- src/test/java/rocks/zipcode/stringsgalore/StringTheoryTest.java
+++ src/test/java/rocks/zipcode/stringsgalore/StringTheoryTest.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.stringsgalore;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringTheoryTest {
@@ -12,5 +15,54 @@ public class StringTheoryTest {
 
     @org.junit.After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getCharacterFromStringTest() {
+        //Given
+        String string = "Java Exercise!";
+        Character expected = 'J';
+        Character expected2 = 'i';
+
+        //When
+        Character actual = st.characterByIndex(string, 0);
+        Character actual2 = st.characterByIndex(string, 10);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected2, actual2);
+
+    }
+
+    @Test
+    public void getCharacterUnicodeFromStringTest() {
+        //Given
+        String string = "w3resource.com";
+        Character expected = 51;
+        Character expected2 = 101;
+
+        //When
+        Character actual = st.characterUnicodeByIndex(string, 1);
+        Character actual2 = st.characterUnicodeByIndex(string, 9);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected2,actual2);
+    }
+
+    @Test
+    public void getCharacterUnicodeBeforeSpecifiedIndexTest() {
+        //Given
+        String string = "w3resource.com";
+        Character expected = 119;
+        Character expected2 = 99;
+
+        //When
+        Character actual = st.characterUnicodeBeforeSpecifiedIndex(string, 1);
+        Character actual2 = st.characterUnicodeBeforeSpecifiedIndex(string, 9);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected2,actual2);
     }
 }
